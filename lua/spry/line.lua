82d16e40b03ee30687b087d84c2e7bc91158029c
Mark Garrigan <mrkgrrgn@gmail.com>
2024-08-20 22:57:01 -0500

    seriously

--- lua/spry/line.lua
+++ lua/spry/line.lua
@@ -17,52 +17,52 @@ M.setup = function(config)
 end
 
 M.tabLine = function()
-	local s = ""
-	local padding = " " -- Adjust the padding here
-	local separator = "│" -- Vertical bar as the separator
+  local s = ""
+  local padding = " " -- Adjust the padding here
+  local separator = "│" -- Vertical bar as the separator
 
-	for i = 1, vim.fn.tabpagenr("$") do
-		-- Add a separator between tabs
-		if i > 1 then
-			s = s .. "%#TabLineSeparator#" .. separator
-		end
+  for i = 1, vim.fn.tabpagenr("$") do
+    -- Add a separator between tabs
+    if i > 1 then
+      s = s .. "%#TabLineSeparator#" .. separator
+    end
 
-		-- Check if this is the active tab
-		if i == vim.fn.tabpagenr() then
-			-- Active tab with custom highlighting
-			s = s .. "%#TabLineSel#"
-		else
-			-- Inactive tabs
-			s = s .. "%#TabLine#"
-		end
+    -- Check if this is the active tab
+    if i == vim.fn.tabpagenr() then
+      -- Active tab with custom highlighting
+      s = s .. "%#TabLineSel#"
+    else
+      -- Inactive tabs
+      s = s .. "%#TabLine#"
+    end
 
-		-- Get the buffer list and window number
-		local buflist = vim.fn.tabpagebuflist(i)
-		local winnr = vim.fn.tabpagewinnr(i)
+    -- Get the buffer list and window number
+    local buflist = vim.fn.tabpagebuflist(i)
+    local winnr = vim.fn.tabpagewinnr(i)
 
-		-- Get the buffer name or set it to "No Name"
-		local filename = vim.fn.bufname(buflist[winnr])
-		if filename == "" then
-			filename = "• • •"
-		else
-			filename = vim.fn.fnamemodify(filename, ":t")
-		end
+    -- Get the buffer name or set it to "No Name"
+    local filename = vim.fn.bufname(buflist[winnr])
+    if filename == "" then
+      filename = "• • •"
+    else
+      filename = vim.fn.fnamemodify(filename, ":t")
+    end
 
-		-- Tab name with padding
-		s = s .. padding .. filename .. padding
+    -- Tab name with padding
+    s = s .. padding .. filename .. padding
 
-		-- Modified flag
-		if vim.fn.getbufvar(buflist[winnr], "&modified") == 1 then
-			s = s .. " +"
-		end
+    -- Modified flag
+    if vim.fn.getbufvar(buflist[winnr], "&modified") == 1 then
+      s = s .. " +"
+    end
 
-		s = s .. " %T"
-	end
+    s = s .. " %T"
+  end
 
-	-- Avoid the last tab taking up the remainder of the space
-	s = s .. "%#TabLineFill#%="
+  -- Avoid the last tab taking up the remainder of the space
+  s = s .. "%#TabLineFill#%="
 
-	return s
+  return s
 end
 
 return M
